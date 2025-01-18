b7eb0b012862feb088459f7bc70fa57c6a915933
besm1 <besn1@yandex.ru>
2025-01-18 15:38:19 +0300

    Admin page added

--- GameShop/task1/admin.py
+++ GameShop/task1/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Buyer, Game
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size',)
+    search_fields = ('title',)
+    list_filter = ('size', 'cost',)
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age',)
+    search_fields = ('name', )
+    list_filter = ('balance', 'age',)
+    readonly_fields = ('balance',)
+    list_per_page = 30
